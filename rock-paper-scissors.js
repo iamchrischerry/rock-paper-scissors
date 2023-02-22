bb8f42b44a066e70e36592fe1180f0fcf4ea0f2a
iamchrischerry <chrisc1607@hotmail.com>
2023-02-22 09:58:06 +0100

    Add 3 functions to get the computer and player choice working

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,2 +1,32 @@
-let cheese = 123456;
-console.log(cheese)
+const options = ["rock", "paper", "scissors"]
+
+function getComputerChoice() {
+    let computerChoice = options[Math.floor(Math.random() * options.length)];
+    console.log(computerChoice);
+    return computerChoice;
+}
+const playerChoice = prompt ("Choose your weapon.")
+console.log(playerChoice);
+
+function checkWinner(computerChoice, playerChoice) {
+    if (playerChoice == computerChoice) {
+        return "Tie";
+    }
+    else {
+        return "No Tie"
+    }
+}
+
+function playRound(playerChoice, computerChoice){
+    const result = checkWinner(playerChoice, computerChoice);{
+        if (result == "Tie"){
+            return "It's a Tie!"
+        }
+        else{
+            return "No Tie!"
+        }
+    }
+}
+const computerChoice = getComputerChoice();
+console.log(playRound(playerChoice,computerChoice));
+
